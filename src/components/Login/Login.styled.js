3578101dcb79b80e0c83631bdcde90c7e74500e3
Marc McCarthy <marstheory@gmail.com>
2023-02-21 18:02:38 -0600

    added spacing for buttons.

--- src/components/Login/Login.styled.js
+++ src/components/Login/Login.styled.js
@@ -43,7 +43,7 @@ export const TellMeButton = styled.button`
 	background-color: green;
 	border: 1px solid #000000;
 	font-weight: bold;
-	margin-top: 1rem;
+	margin: 2rem;
 	&:active {
 		transform: scale(0.96);
 		box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.25);
